e3255b5e127d815377e3dc4b97a6f6b9abe72fa3
tyxodus04 <tyxodus04@gmail.com>
2023-09-04 18:30:44 +0900

    add file mod

---
+++ PY-LECTURE/edittest.py
@@ -0,0 +1,16 @@
+a = "hello"
+b = "world"
+print(a, b)
+
+print(a)
+print(b)
+print(b, a)
+print("python", a, b)
+
+number_school = 2394584903
+str_name = "python"
+bool_view = False
+flt_pi = 3.14
+char_element = 'a'
+chars = "hello world"
+print("number : ", number_school, " name : ", str_name, " bool : ", bool_view, " float : " , flt_pi, " chatcter : ", char_element, "chatcters : ", chars)
